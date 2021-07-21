09f38e9241ecfe5c4a172248f568758f21f15dfa
unknown <abanoubsamir@gmail.com>
2021-07-21 11:45:37 +0200

    1

--- database/seeders/examsSeeder.php
+++ database/seeders/examsSeeder.php
@@ -14,19 +14,19 @@ class examsSeeder extends Seeder
      */
     public function run()
     {
-        // for ($e = 1; $e <= 8; $e++) {
-        //     for ($i = 1; $i <= 3; $i++) {
-        //         Exam::create([
-        //             'name'=>"exam$i",
-        //             'desc'=>"Pro eu error molestie deserunt. At per viderer bonorum persecuti.",
-        //             'img' =>"/assets/exam img/exam$e.jpg",
-        //             'skill_id'=> $e,
-        //             'questions_no'=>'7',
-        //             'difficulty'=>  mt_rand(1, 5),
-        //             'duration_mins'=>  mt_rand(1, 3)*30
-        //             ]);
-        //         }
-        //     }
+        for ($e = 1; $e <= 8; $e++) {
+            for ($i = 1; $i <= 3; $i++) {
+                Exam::create([
+                    'name'=>"exam$i",
+                    'desc'=>"Pro eu error molestie deserunt. At per viderer bonorum persecuti.",
+                    'img' =>"/assets/exam img/exam$e.jpg",
+                    'skill_id'=> $e,
+                    'questions_no'=>'7',
+                    'difficulty'=>  mt_rand(1, 5),
+                    'duration_mins'=>  mt_rand(1, 3)*30
+                    ]);
+                }
+            }
 
         $exams = Exam::get();  
         $i=1;
